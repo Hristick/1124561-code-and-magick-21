bd8d7570802e7636ed716d4e8e40e1d177a0d129
Hristick <hristick@mail.ru>
2020-09-24 21:09:41 +0300

    module2-task1 v2

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,79 @@
 'use strict';
 
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 200;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 50;
+const FONT_GAP = 15;
+const TEXT_OFSET = 50;
+const TEXT_WIDTH = 100;
+const BAR_HEIGHT = 150;
+const BAR_WIDTH = 40;
+const SHADOW_OFSET = 10;
+const MY_WIN_COLOR = 'rgba(255, 0, 0, 1)';
+const CLOUD_SHADOW = 'rgba(0, 0, 0, 0.7)';
+const CLOUD_BACKGROUND = '#fff';
 
-var renderCloud = function(ctx, x, y, color) {
+
+let renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx) {
-  renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, 100, 50, '#fff');
+let getMaxElement = function(arr) {
+  let maxElement = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
 };
+
+window.renderStatistics = function(ctx, players, times, color) {
+  renderCloud(
+    ctx,
+    CLOUD_X + SHADOW_OFSET,
+    CLOUD_Y + SHADOW_OFSET,
+    CLOUD_SHADOW
+  );
+  renderCloud(
+    ctx,
+    CLOUD_X,
+    CLOUD_Y,
+    CLOUD_BACKGROUND
+  );
+
+  ctx.fillStyle = '#000';
+  ctx.font = "PT Mono 48px";
+  ctx.fillText("Ура вы победили!", GAP * 2 + FONT_GAP, GAP);
+  ctx.fillText("Список результатов: ", GAP * 2 + FONT_GAP, GAP + FONT_GAP);
+
+
+  let maxTime = getMaxElement(times);
+
+  for (let i = 0; i < players.length; i++) {
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = MY_WIN_COLOR;
+    } else {
+      ctx.fillStyle = `hsl(255, 100%,` + Math.random() * 100 + `%)`;
+    }
+    ctx.fillRect(
+      TEXT_OFSET + TEXT_WIDTH + (TEXT_WIDTH * i),
+      TEXT_WIDTH * 2 + GAP,
+      BAR_WIDTH, (BAR_HEIGHT * times[i] / maxTime) * -1)
+
+      ctx.fillStyle = '#000';
+      ctx.font = "PT Mono 48px";
+      ctx.fillText(
+        players[i],
+        TEXT_OFSET + TEXT_WIDTH + (TEXT_WIDTH * i),
+        CLOUD_HEIGHT
+        );
+
+        ctx.fillText(Math.floor(times[i]), TEXT_OFSET + TEXT_WIDTH + (TEXT_WIDTH * i), CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - (GAP - FONT_GAP));
+      };
+    };
